d428a8a4b535da13392a22548ca4287b7017af32
dufwndl1945 <dufwndl1945@naver.com>
2017-03-21 15:42:41 +0900

    Update Average.java

--- Average.java
+++ Average.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
-public class Average { 
+public class Average { 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.print("입력할 최대 실수의 갯수를 입력하세요. >> ");    
-        Scanner input = new Scanner(System.in);  
-        int size = input.nextInt();    
-        System.out.println("\n중간에 0을 입력하면 이전 입력자료로 계산하고 종료합니다.");  
-        System.out.print("실수 " + size + "개를 입력하세요. >> ");    
+        System.out.print("입력할 최대 실수의 갯수를 입력하세요. >> ");    
+        Scanner input = new Scanner(System.in);  
+        int size = input.nextInt();    
+        System.out.println("\n중간에 0을 입력하면 이전 입력자료로 계산하고 종료합니다.");  
+        System.out.print("실수 " + size + "개를 입력하세요. >> ");    
+        double abb[] = new double[size];
         
-        //프로그래밍 하세요.
+        for(size =1; size<100; size++){
+        	abb        	}
+        
+        
+        
+        //프로그래밍 하세요.
         
         System.out.println("합: " + sum + ", 평균: " + sum/size);
     }
